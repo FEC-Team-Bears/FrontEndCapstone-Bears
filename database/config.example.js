23536e6a7ecb4e3caf81fa18c458db5dbd385774
Cindy Ryoo <cindyryoo7@gmail.com>
2021-02-28 12:53:57 -0600

    Set up initial client, server, and database files and initialized settings

---
+++ database/config.example.js
@@ -0,0 +1,12 @@
+/*
+ * INSTURCTIONS:
+ * Create a copy of this file and name it "config.js" in the same database folder.
+ * Enter your mysql credentials.
+ */
+
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  password: 'FILL_ME_IN',
+  database: 'FILL_ME_IN'
+};
