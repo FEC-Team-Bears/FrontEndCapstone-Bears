75d39c72602f8953a46c7ab07291c1163f2c3889
John Cooke <johncooke832@gmail.com>
2021-03-02 13:19:36 -0600

    Render related products to the page to make sure you have relevant data (AKA picture, name, category, price)

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import $ from 'jquery';
 import axios from 'axios';
 import Related_products from './Related_products/Related_products.jsx';
+import Related_product_card from './Related_products/Related_product_card.jsx'
 import Your_outfit from './Your_outfit/Your_outfit.jsx';
 
 const App = (prop) => {
