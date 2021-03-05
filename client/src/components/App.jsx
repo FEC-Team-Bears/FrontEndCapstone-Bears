5f574f71a4406f8c4a9891a8bd078767c942b785
Matt <matt.collins087@gmail.com>
2021-03-05 15:34:19 -0600

    Fixed es-lint changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import $ from 'jquery';
 import axios from 'axios';
-import Related_products from './Related_products/Related_products.jsx';
-import Your_outfit from './Your_outfit/Your_outfit.jsx';
-import RatingsReviews from './Reviews_Ratings/Ratings_Reviews.jsx';
+import RatingsReviews from './ReviewsRatings/RatingsReviews.jsx';
 import API_KEY from '/config.js';
 
 const App = () => {
@@ -15,11 +13,11 @@ const App = () => {
         'Authorization': API_KEY
       }
     })
-    .then(response => {
-      changeCurrentProductId(response.data.id)
-    })
-    .catch((error) => console.error(error))
-  }
+      .then(response => {
+        changeCurrentProductId(response.data.id);
+      })
+      .catch((error) => console.error(error));
+  };
 
   useEffect(() => {
     axiosGetProductId(currentProductId);
@@ -28,7 +26,7 @@ const App = () => {
   return (
     <div>
 
-      <RatingsReviews productId={currentProductId} changeId={changeCurrentProductId}/>
+      <RatingsReviews productId={currentProductId} changeId={changeCurrentProductId} />
 
 
     </div>
---
+++ client/src/components/ReviewsRatings/RatingsReviews.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import API_KEY from '/config.js';
+import ReviewList from './ReviewList.jsx';
+
+const RatingsReviews = ({ productId, changeId }) => {
+  const [reviews, getAllReviews] = useState([]);
+  const [reviewMetaData, useReviewMetaData] = useState(null);
+
+  const axiosGetAllReviews = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews', {
+      headers: {
+        'Authorization': API_KEY
+      },
+      params: {
+        'product_id': `${productId}`
+      }
+    })
+      .then(reviews => {
+        // console.log(reviews.data.results);
+        getAllReviews(reviews.data.results);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const axiosGetAllMetaData = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/meta', {
+      headers: {
+        'Authorization': API_KEY
+      },
+      params: {
+        'product_id': `${productId}`
+      }
+    })
+      .then(reviews => {
+        // console.log(reviews.data);
+        useReviewMetaData(reviews.data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+
+
+  useEffect(() => {
+    axiosGetAllReviews();
+    axiosGetAllMetaData();
+
+  }, [productId]);
+
+  return (
+    <div>
+      {reviewMetaData ?
+        <div>
+          <ReviewList reviews={reviews} productId={productId} reviewChar={reviewMetaData.characteristics} />
+          <button onClick={() => { changeId(21114); }}>Hello</button>
+        </div>
+        : null
+      }
+    </div>
+
+  );
+};
+
+export default RatingsReviews;
