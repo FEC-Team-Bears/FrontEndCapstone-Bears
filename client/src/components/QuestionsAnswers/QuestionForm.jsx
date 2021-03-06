4fefdd28f1c9a8540d485fb86e7fc6bfc3fef530
Cindy Ryoo <cindyryoo7@gmail.com>
2021-03-06 17:38:38 -0600

    Refactored Question Form to validate entries before submission and create new question on submit

--- client/src/components/QuestionsAnswers/QuestionForm.jsx
+++ client/src/components/QuestionsAnswers/QuestionForm.jsx
@@ -3,19 +3,68 @@ import axios from 'axios';
 import API_KEY from '/config.js';
 import { Modal, Button, Form, InputGroup } from 'react-bootstrap';
 
-const QuestionForm = ({ productId, length }) => {
+const QuestionForm = ({ productId, length, handleNewQuestion }) => {
   const [show, setShow] = useState(false);
   const [product, setProduct] = useState('');
+  const [validated, setValidated] = useState(false);
   const [question, setQuestion] = useState('');
   const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
-  const [validated, setValidated] = useState(false);
+
+  const getProductName = () => {
+    axios
+      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${productId}`, {
+        headers: {
+          'Authorization': API_KEY
+        }
+      })
+      .then(response => {
+        setProduct(response.data.name);
+      })
+      .catch(err => {
+        console.log('Error: cannot retrieve product information');
+      });
+  };
+  const submitQuestion = () => {
+    axios
+      .post('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/qa/questions',
+        {
+          'product_id': productId,
+          'body': question,
+          'name': nickname,
+          'email': email
+        },
+        {
+          headers: {
+            'Authorization': API_KEY
+          }
+        })
+      .then(response => {
+        const newQuestion = [{
+          'question_id': null,
+          'question_body': question,
+          'question_date': null,
+          'asker_name': nickname,
+          'question_helpfulness': 0,
+          'reported': false,
+          'answers': {},
+        }];
+        handleNewQuestion(newQuestion);
+      })
+      .catch(err => {
+        console.log('Error: cannot add question');
+      });
+  };
 
   const handleShow = () => {
     setShow(true);
   };
   const handleClose = () => {
-    setShow(false);
+    if (question.concat(nickname, email) === '') {
+      setShow(false);
+    } else if (confirm('Are you sure you want to exit? Your changes will not be saved.')) {
+      setShow(false);
+    }
   };
   const handleQuestionChange = (e) => {
     setQuestion(e.target.value);
@@ -28,37 +77,24 @@ const QuestionForm = ({ productId, length }) => {
   };
   const handleSubmit = (e) => {
     const form = e.currentTarget;
-    if (form.checkValdity() === false) {
+    if (form.checkValidity() === false) {
       e.preventDefault();
+      e.stopPropagation();
+    } else {
+      e.preventDefault();
+      setShow(false);
+      submitQuestion();
     }
     setValidated(true);
-    // submit axios request
-  };
-  const getProductName = (productId) => {
-    axios
-      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products?product_id=${productId}`, {
-      // .get('https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/21113', {
-        headers: {
-          'Authorization': API_KEY
-        }
-      })
-      .then(response => {
-        setProduct(response.data.name);
-      })
-      .catch(err => {
-        console.log('Error: cannot retrieve product information');
-      });
   };
 
   useEffect(() => {
-    getProductName(productId);
-    // getProductName();
+    getProductName();
   }, []);
 
   return (
     <div>
       {(!length) ? <Button onClick={handleShow}>Submit a New Question</Button> : <Button onClick={handleShow}>Add a Question + </Button>}
-
       <Modal show={show} onHide={handleClose} backdrop='static' keyboard={false} centered>
         <Modal.Header closeButton>
           <Modal.Title>
@@ -73,13 +109,13 @@ const QuestionForm = ({ productId, length }) => {
               <Form.Label>What is your question?*</Form.Label>
               <InputGroup hasValidation>
                 <Form.Control
-                  required
                   as='textarea'
                   placeholder='Write your question here'
                   maxLength='1000'
                   rows={3}
-                  onChange={handleQuestionChange} />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  onChange={handleQuestionChange}
+                  required />
+                <Form.Control.Feedback type='invalid'>Please enter a question.</Form.Control.Feedback>
               </InputGroup>
             </Form.Group>
             <br />
@@ -87,12 +123,12 @@ const QuestionForm = ({ productId, length }) => {
               <Form.Label>What is your nickname?*</Form.Label>
               <InputGroup hasValidation>
                 <Form.Control
-                  required
                   type='text'
                   placeholder='Example: jackson11!'
                   maxLength='60'
-                  onChange={handleNicknameChange} />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  onChange={handleNicknameChange}
+                  required />
+                <Form.Control.Feedback type='invalid'>Please enter a username.</Form.Control.Feedback>
               </InputGroup>
               <Form.Text>For privacy reasons, do not use your full name or email address.</Form.Text>
             </Form.Group>
@@ -101,24 +137,20 @@ const QuestionForm = ({ productId, length }) => {
               <Form.Label>What is your email?*</Form.Label>
               <InputGroup hasValidation>
                 <Form.Control
-                  required
                   type='email'
                   placeholder='Example: jack@email.com'
                   maxLength='60'
-                  onChange={handleEmailChange} />
+                  onChange={handleEmailChange}
+                  required />
                 <Form.Control.Feedback type='invalid'>Please provide a valid email format.</Form.Control.Feedback>
               </InputGroup>
               <Form.Text>For authentication reasons, you will not be emailed.</Form.Text>
             </Form.Group>
+            <Button variant='danger' onClick={handleClose}>Close</Button>{' '}
+            <Button variant='primary' type='submit'>Submit</Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant='primary' type='submit'>Submit</Button>
-        </Modal.Footer>
       </Modal>
-
-
-
     </div>
   );
 };
--- client/src/components/QuestionsAnswers/QuestionsList.jsx
+++ client/src/components/QuestionsAnswers/QuestionsList.jsx
@@ -4,48 +4,50 @@ import API_KEY from '/config.js';
 import Question from './Question.jsx';
 import QuestionForm from './QuestionForm.jsx';
 
-const QuestionsList = ({productId}) => {
+const QuestionsList = ({ productId }) => {
   const [count, setCount] = useState(4);
   const [questions, setQuestions] = useState([]);
   const [page, setPage] = useState(1);
   const [remainingQ, setRemainingQ] = useState(true);
 
-  const getAllQuestions = (productId) => {
+  const getAllQuestions = (currentPage = page, currentQuestions = questions) => {
     axios
-      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/qa/questions?product_id=${productId}&page=${page}`, {
-      // .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/qa/questions?product_id=21113&page=${page}`, {
+      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/qa/questions?product_id=${productId}&page=${currentPage}&count=97`, {
         headers: {
           'Authorization': API_KEY
         }
       })
       .then(response => {
-        if (response.data.results.length !== 0) {
-          const allQuestions = questions.concat(response.data.results);
+        if (response.data.results.length < count || response.data.results.length === 0) {
+          setRemainingQ(false);
+        } else {
+          const allQuestions = currentQuestions.concat(response.data.results);
           allQuestions.sort((a, b) => {
             (a.question_helpfulness > b.question_helpfulness) ? 1 : (a.question_helpfulness === b.question_helpfulness) ? ((a.question_id > b.question_id) ? 1 : -1 ) : -1;
           });
           setQuestions(allQuestions);
           setPage(page + 1);
-        } else {
-          setRemainingQ(false);
         }
       })
       .catch(err => {
         console.log('Error: Cannot retrieve questions from API');
       });
   };
+
   const showMoreQuestions = () => {
     setCount(count + 2);
   };
+  const handleNewQuestion = (newQuestion) => {
+    const allQuestions = newQuestion.concat(questions);
+    setQuestions(allQuestions);
+  };
 
   useEffect(() => {
-    getAllQuestions(productId);
-    // getAllQuestions();
-  }, []);
+    getAllQuestions(1, []);
+  }, [productId]);
   useEffect(() => {
-    if (count > questions.length) {
-      getAllQuestions(productId);
-      // getAllQuestions();
+    if (count >= questions.length) {
+      getAllQuestions();
     }
   }, [count]);
 
@@ -55,7 +57,7 @@ const QuestionsList = ({productId}) => {
         <Question key={question.question_id} question={question}/>
       )) : null}
       {(questions.length > 2 && remainingQ) ? <button onClick={showMoreQuestions}>More Answered Questions</button> : null}
-      <QuestionForm productId={productId} length={questions.length}/>
+      <QuestionForm productId={productId} length={questions.length} handleNewQuestion={handleNewQuestion}/>
     </div>
   );
 };
