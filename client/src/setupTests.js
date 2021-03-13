14c97d5e15392cfd9759299e27c3d52f293b476f
Steve <steve.gackstetter@gmail.com>
2021-03-13 14:54:09 -0600

    adding to testing

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,13 +3,23 @@ import Adapter from 'enzyme-adapter-react-16';
 import { mount, shallow, render } from 'enzyme';
 configure({
   adapter: new Adapter(),
-  disableLifecycleMethods: true });
+  disableLifecycleMethods: true
+});
 
 import Overview from './components/Overview/Overview.jsx';
+
+
 describe('MyComponent', () => {
   it('should render correctly in "debug" mode', () => { // example stuff
-    const component = shallow(<MyComponent debug />);
+    const component = shallow(<Overview debug />);
 
     expect(component).toMatchSnapshot();
   });
+
+  it('should render banner text correctly with given strings', () => {
+    const productId = '21111';
+    const component = shallow(<Overview productId={productId} />);
+
+    expect(component).toMatchSnapshot();
+
 });
