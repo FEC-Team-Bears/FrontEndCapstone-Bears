759724436af7b5c9994a1e40406611d4df93a0e4
Matt Collins <70385445+matt-collins087@users.noreply.github.com>
2021-03-13 15:16:26 -0600

    Revert "Revert 26 sg overview component"

---
+++ client/src/components/Overview/Overview.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import API_KEY from '../../../../config.js';
+import PrimaryImage from './PrimaryImage.jsx';
+import ProductDetails from './ProductDetails.jsx';
+
+var Overview = ({ productId, productDetails, getStyles, styles, reviews }) => {
+
+  const [product, getProduct] = useState([]);
+  const [style, getStyle] = useState(0);
+
+  let fetchProduct = (product = '21111') => {
+    axios
+      .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products/${product}/styles`, {
+        headers: {
+          'Authorization': API_KEY
+        }
+      })
+      .then((results) => {
+        getProduct(results.data.results[style].photos);
+      })
+      .catch((err) => {
+        console.error('there was an error in your axios call: ', err);
+      });
+  };
+
+  const setStyle = (style) => {
+    getStyle(style);
+  };
+
+  useEffect(() => {
+    fetchProduct(productId);
+  }, [productId, style]);
+
+  return (
+    <div className="container overview-container">
+      <div className="row">
+        <div className="col-8 main-pic">
+          <PrimaryImage
+            product={ product }
+            style={ style }
+            setStyle={ setStyle }/>
+        </div>
+        <div className="col-4 main-details">
+          <ProductDetails
+            reviews={ reviews }
+            productDetails={ productDetails }
+            style={ style }
+            styles={ styles }
+            setStyle={ setStyle }/>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="row">
+        <div className="col-8">
+          <h3>{ productDetails.slogan }</h3>
+          { productDetails.description }
+        </div>
+        <div className="col-4 feature-div">
+          {productDetails.features ? productDetails.features.map((feature, index) => {
+            return (<div className="feature-span" key={ index }>&#10004; {feature.feature}: {feature.value}</div>);
+          }) : ''}
+        </div>
+      </div>
+    </div>
+  );
+
+};
+
+export default Overview;
---
+++ client/src/components/ReviewsRatings/Review.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Moment from 'moment';
+import StarRating from './StarRating.jsx';
+import ReviewPhotos from './ReviewPhotos.jsx';
+import axios from 'axios';
+import API_KEY from '/config.js';
+
+const Review = ({ review }) => {
+  const [recommendCount, setRecommendCount] = useState(review.helpfulness);
+  const [userThoughtHelpful, setUserThoughtHelpful] = useState(false);
+
+  const axiosUpdateRecommended = () => {
+    axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/${review.review_id}/helpful`, { helpfulness: 0 }, {
+      headers: {
+        'Authorization': API_KEY
+      },
+    })
+      .then(reviews => {
+        !userThoughtHelpful ? setRecommendCount(recommendCount + 1) : null;
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="single-review-container">
+      <div className="review-date-container">
+        <StarRating review={review} />
+        <p>{Moment(review.date).format('MMMM DD, YYYY')}</p>
+      </div>
+      <h3 className="header-color">{review.summary}</h3>
+      <p>{review.body}</p>
+      {(review.response !== null && review.response.length > 0) ?
+        <div className="review-response">
+          <h5>Response:</h5>
+          <p>{review.response}</p>
+        </div>
+        : null}
+      {review.photos.map((photo, index) => {
+        return <ReviewPhotos key={index} photo={photo} />;
+      })}
+      <p>Helpful? <span className="clickable-color" onClick={() => {
+        setUserThoughtHelpful(true);
+        axiosUpdateRecommended();
+      }
+      }><span>Yes</span></span> ({recommendCount})</p>
+    </div>
+  );
+};
+
+export default Review;
